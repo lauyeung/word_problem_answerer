c06616ca720d71f5e979e460e193e89f95cf6406
Louise Au Yeung <louise@eabx.org>
2013-09-15 08:47:30 -0400

    initcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WordProblemAnswererRails::Application.config.secret_key_base = '5c7f22f825d89eb0bdb2a0a07d21d71b329c67e5c1baad438f48d7d5df8fe7226c341619bfe331e2cf19ea74afa22d9cdecee57fb0c1a0afc44d6c1e2126d877'
